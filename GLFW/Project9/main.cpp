0bf2135028c7a19e3255a3123b54a6c7d1fcaff4
BryceX <btau20102@yahoo.com>
2015-01-06 15:51:11 -0800

    Blue Triangle

---
+++ GLFW/Project9/main.cpp
@@ -0,0 +1,252 @@
+#include <GL/glew.h>
+#include "wglew.h"
+#include <GLFW/glfw3.h>
+#include <vector>
+#include <string>
+#include <fstream>
+
+int screenSize = 1024;
+float* getOrtho(float left, float right, float bottom, float top, float a_fNear, float a_fFar)
+{
+	//to correspond with mat4 in the shader
+	//ideally this function would be part of your matrix class
+	//however I wasn't willing to write your matrix class for you just to show you this
+	//so here we are in array format!
+	//add this to your matrix class as a challenge if you like!
+	float* toReturn = new float[12];
+	toReturn[0] = 2.0 / (right - left);;
+	toReturn[1] = toReturn[2] = toReturn[3] = toReturn[4] = 0;
+	toReturn[5] = 2.0 / (top - bottom);
+	toReturn[6] = toReturn[7] = toReturn[8] = toReturn[9] = 0;
+	toReturn[10] = 2.0 / (a_fFar - a_fNear);
+	toReturn[11] = 0;
+	toReturn[12] = -1 * ((right + left) / (right - left));
+	toReturn[13] = -1 * ((top + bottom) / (top - bottom));
+	toReturn[14] = -1 * ((a_fFar + a_fNear) / (a_fFar - a_fNear));
+	toReturn[15] = 1;
+	return toReturn;
+}
+//declare our vertex structure
+struct Vertex
+{
+	float fPositions[4];
+	float fColours[4];
+};
+
+GLuint CreateShader(GLenum a_eShaderType, const char *a_strShaderFile)
+{
+	std::string strShaderCode;
+	//open shader file
+	std::ifstream shaderStream(a_strShaderFile);
+	//if that worked ok, load file line by line
+	if (shaderStream.is_open())
+	{
+		std::string Line = "";
+		while (std::getline(shaderStream, Line))
+		{
+			strShaderCode += "\n" + Line;
+		}
+		shaderStream.close();
+	}
+
+	//convert to cstring
+	char const *szShaderSourcePointer = strShaderCode.c_str();
+
+	//create shader ID
+	GLuint uiShader = glCreateShader(a_eShaderType);
+	//load source code
+	glShaderSource(uiShader, 1, &szShaderSourcePointer, NULL);
+
+	//compile shader
+	glCompileShader(uiShader);
+
+	//check for compilation errors and output them
+	GLint iStatus;
+	glGetShaderiv(uiShader, GL_COMPILE_STATUS, &iStatus);
+	if (iStatus == GL_FALSE)
+	{
+		GLint infoLogLength;
+		glGetShaderiv(uiShader, GL_INFO_LOG_LENGTH, &infoLogLength);
+
+		GLchar *strInfoLog = new GLchar[infoLogLength + 1];
+		glGetShaderInfoLog(uiShader, infoLogLength, NULL, strInfoLog);
+
+		const char *strShaderType = NULL;
+		switch (a_eShaderType)
+		{
+		case GL_VERTEX_SHADER: strShaderType = "vertex"; break;
+		case GL_FRAGMENT_SHADER: strShaderType = "fragment"; break;
+		}
+
+		fprintf(stderr, "Compile failure in %s shader:\n%s\n", strShaderType, strInfoLog);
+		delete[] strInfoLog;
+	}
+
+	return uiShader;
+}
+
+GLuint CreateProgram(const char *a_vertex, const char *a_frag)
+{
+	std::vector<GLuint> shaderList;
+
+	shaderList.push_back(CreateShader(GL_VERTEX_SHADER, a_vertex));
+	shaderList.push_back(CreateShader(GL_FRAGMENT_SHADER, a_frag));
+
+	//create shader program ID
+	GLuint uiProgram = glCreateProgram();
+
+	//attach shaders
+	for (auto shader = shaderList.begin(); shader != shaderList.end(); shader++)
+		glAttachShader(uiProgram, *shader);
+
+	//link program
+	glLinkProgram(uiProgram);
+
+	//check for link errors and output them
+	GLint status;
+	glGetProgramiv(uiProgram, GL_LINK_STATUS, &status);
+	if (status == GL_FALSE)
+	{
+		GLint infoLogLength;
+		glGetProgramiv(uiProgram, GL_INFO_LOG_LENGTH, &infoLogLength);
+
+		GLchar *strInfoLog = new GLchar[infoLogLength + 1];
+		glGetProgramInfoLog(uiProgram, infoLogLength, NULL, strInfoLog);
+		fprintf(stderr, "Linker failure: %s\n", strInfoLog);
+		delete[] strInfoLog;
+	}
+
+	for (auto shader = shaderList.begin(); shader != shaderList.end(); shader++)
+	{
+		glDetachShader(uiProgram, *shader);
+		glDeleteShader(*shader);
+	}
+
+	return uiProgram;
+}
+
+int main()
+{
+	//Initialise GLFW
+	if (!glfwInit())
+	{
+		return -1;
+	}
+	//create some vertices
+	Vertex* myShape = new Vertex[3];
+	myShape[0].fPositions[0] = screenSize / 2.0f;
+	myShape[0].fPositions[1] = screenSize / 3.0 + 10.0f;
+	myShape[1].fPositions[0] = screenSize / 2 - 5.0f;
+	myShape[1].fPositions[1] = screenSize / 3.0 - 10.0f;
+	myShape[2].fPositions[0] = screenSize / 2 + 5.0f;
+	myShape[2].fPositions[1] = screenSize / 3.0 - 10.0f;
+	for (int i = 0; i < 3; i++)
+	{
+		myShape[i].fPositions[2] = 0.0f;
+		myShape[i].fPositions[3] = 1.0f;
+		myShape[i].fColours[0] = 0.0f;
+		myShape[i].fColours[1] = 0.0f;
+		myShape[i].fColours[2] = 1.0f;
+		myShape[i].fColours[3] = 1.0f;
+	}
+
+	//create a windowed mode window and it's OpenGL context
+	GLFWwindow* window;
+	window = glfwCreateWindow(1024, 1024, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	//make the window's context current
+	glfwMakeContextCurrent(window);
+
+	//………Insert this into the main.cpp example given in the setting up GLFW section – 
+	//once the glfw window has been created and after 'glfwMakeContextCurrent(window);'
+	// start GLEW
+	if (glewInit() != GLEW_OK)
+	{
+		// OpenGL didn't start-up! shutdown GLFW and return an error code
+		glfwTerminate();
+		return -1;
+	}
+	//create ID for a vertex buffer object
+	GLuint uiVBO;
+	glGenBuffers(1, &uiVBO);
+
+	//check it succeeded
+	if (uiVBO != 0)
+	{
+		//bind VBO
+		glBindBuffer(GL_ARRAY_BUFFER, uiVBO);
+		//allocate space for vertices on the graphics card
+		glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex)* 3, NULL, GL_STATIC_DRAW);
+		//get pointer to allocated space on the graphics card
+		GLvoid* vBuffer = glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY);
+		//copy data to graphics card
+		memcpy(vBuffer, myShape, sizeof(Vertex)* 3);
+		//unmap and unbind buffer
+		glUnmapBuffer(GL_ARRAY_BUFFER);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+	}
+	//………
+	const float vertexPositions[] =
+	{
+		screenSize/ 2.0, screenSize / 3.0 + 10.0f, 0.0f, 1.0f,
+		screenSize/2 - 5.0f, screenSize / 3.0 - 10.0f, 0.0f, 1.0f,
+		screenSize/2 + 5.0f, screenSize / 3.0 - 10.0f, 0.0f, 1.0f,
+	};
+
+	const float vertexColours[] =
+	{
+		1.0f, 0.0f, 0.0f, 0.0f,
+		1.0f, 0.0f, 0.0f, 1.0f,
+		1.0f, 0.0f, 0.0f, 1.0f,
+	};
+
+	//create shader program
+	GLuint uiProgramFlat = CreateProgram("VertexShader.glsl", "FlatFragmentShader.glsl");
+
+	//find the position of the matrix variable in the shader so we can send info there later
+	GLuint MatrixIDFlat = glGetUniformLocation(uiProgramFlat, "MVP");
+
+	//set up the mapping of the screen to pixel co-ordinates. Try changing these values to see what happens.
+	float *orthographicProjection = getOrtho(0, 1024, 0, 720, 0, 100);
+	//loop until the user closes the window
+	while (!glfwWindowShouldClose(window))
+	{
+		//draw code goes here
+		glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		//enable shaders
+		glUseProgram(uiProgramFlat);
+
+		glBindBuffer(GL_ARRAY_BUFFER, uiVBO);
+
+		//send our orthographic projection info to the shader
+		glUniformMatrix4fv(MatrixIDFlat, 1, GL_FALSE, orthographicProjection);
+
+		//enable the vertex array state, since we're sending in an array of vertices
+		glEnableVertexAttribArray(0);
+		glEnableVertexAttribArray(1);
+
+		//specify where our vertex array is, how many components each vertex has, 
+		//the data type of each component and whether the data is normalised or not
+		glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), 0);
+		glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(sizeof(float)* 4));
+
+		//draw to the screen
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		//swap front and back buffers
+		glfwSwapBuffers(window);
+
+		//poll for and process events
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
