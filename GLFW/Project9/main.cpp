d1e1a82595baa9cc8aa8f05ded64a7fa2eea3ac6
BryceX <btau20102@yahoo.com>
2015-01-14 12:56:50 -0800

    asdf

--- GLFW/Project9/main.cpp
+++ GLFW/Project9/main.cpp
@@ -214,31 +214,50 @@ int main()
 		stars[i].fColours[3] = 1.0f;
 	}
 	Vertex* asteroids0 = new Vertex[60];
-	asteroids0[0].fPositions[0] = rand() % 1025;
-	asteroids0[0].fPositions[1] = rand() % 1025;
-
-	asteroids0[1].fPositions[0] = asteroids0[0].fPositions[0] + astConstant;
-	asteroids0[1].fPositions[1] = asteroids0[0].fPositions[1] - astConstant;
-
-	asteroids0[2].fPositions[0] = asteroids0[0].fPositions[0] + 2 * astConstant;
-	asteroids0[2].fPositions[1] = asteroids0[0].fPositions[1] - astConstant;
-
-	asteroids0[3].fPositions[0] = asteroids0[0].fPositions[0] + 3 * astConstant;
-	asteroids0[3].fPositions[1] = asteroids0[0].fPositions[1];
-
-	asteroids0[4].fPositions[0] = asteroids0[0].fPositions[0] + 2 * astConstant;
-	asteroids0[4].fPositions[1] = asteroids0[0].fPositions[1] + astConstant;
-
-	asteroids0[5].fPositions[0] = asteroids0[0].fPositions[0] + astConstant;
-	asteroids0[5].fPositions[1] = asteroids0[0].fPositions[1] + astConstant;
-	for (int i = 0; i < 6; i++)
+	
+	for (int i = 0; i < 60; i+=6)
 	{
-		asteroids0[i].fPositions[2] = 0.0f;
-		asteroids0[i].fPositions[3] = 1.0f;
-		asteroids0[i].fColours[0] = 0.0f;
-		asteroids0[i].fColours[1] = 1.0f;
-		asteroids0[i].fColours[0] = 0.0f;
-		asteroids0[i].fColours[1] = 1.0f;
+		// 1st vert
+		asteroids0[0+i].fPositions[0] = rand() % 900;
+		asteroids0[0+i].fPositions[1] = rand() % 900;
+
+		// 2nd vert
+		asteroids0[1+i].fPositions[0] = asteroids0[0+i].fPositions[0] + astConstant;
+		asteroids0[1+i].fPositions[1] = asteroids0[0+i].fPositions[1] - astConstant;
+
+		
+		asteroids0[2+i].fPositions[0] = asteroids0[0+i].fPositions[0] + 2 * astConstant;
+		asteroids0[2+i].fPositions[1] = asteroids0[0+i].fPositions[1] - astConstant;
+
+		asteroids0[3+i].fPositions[0] = asteroids0[0+i].fPositions[0] + 3 * astConstant;
+		asteroids0[3+i].fPositions[1] = asteroids0[0+i].fPositions[1];
+
+	
+
+		asteroids0[4+i].fPositions[0] = asteroids0[0+i].fPositions[0] + 2 * astConstant;
+		asteroids0[4+i].fPositions[1] = asteroids0[0+i].fPositions[1] + astConstant;
+
+		
+
+	
+
+		asteroids0[5 + i].fPositions[0] = asteroids0[0 + i].fPositions[0] + astConstant;
+		asteroids0[5 + i].fPositions[1] = asteroids0[0 + i].fPositions[1] + astConstant;
+
+	
+
+		//asteroids0[1 + i].fPositions[0] = asteroids0[0 + i].fPositions[0] + astConstant;
+	//	asteroids0[1 + i].fPositions[1] = asteroids0[0 + i].fPositions[1] - astConstant;
+	}
+
+	for (int j = 0; j < 60; j++)
+	{
+		asteroids0[j].fPositions[2] = 0.0f;
+		asteroids0[j].fPositions[3] = 1.0f;
+		asteroids0[j].fColours[0] = 0.0f;
+		asteroids0[j].fColours[1] = 1.0f;
+		asteroids0[j].fColours[2] = 0.0f;
+		asteroids0[j].fColours[3] = 0.0f;
 	}
 
 	//create a windowed mode window and it's OpenGL context
@@ -301,9 +320,9 @@ int main()
 	if (uiVBOasteroid != 0)
 	{
 		glBindBuffer(GL_ARRAY_BUFFER, uiVBOasteroid);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex)* 6, NULL, GL_STATIC_DRAW);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex)* 60, NULL, GL_STATIC_DRAW);
 		GLvoid* vBuffer = glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY);
-		memcpy(vBuffer, asteroids0, sizeof(Vertex)* 6);
+		memcpy(vBuffer, asteroids0, sizeof(Vertex)* 60);
 		glUnmapBuffer(GL_ARRAY_BUFFER);
 		glBindBuffer(GL_ARRAY_BUFFER, 0);
 	}
@@ -352,9 +371,9 @@ int main()
 	if (uiIBOasteroids != 0)
 	{
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, uiIBOasteroids);
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(char), NULL, GL_STATIC_DRAW);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, 60 * sizeof(char), NULL, GL_STATIC_DRAW);
 		GLvoid* iBuffer = glMapBuffer(GL_ELEMENT_ARRAY_BUFFER, GL_WRITE_ONLY);
-		for (int i = 0; i < 6; i++)
+		for (int i = 0; i < 60; i++)
 		{
 			((char*)iBuffer)[i] = i;
 		}
@@ -444,12 +463,11 @@ int main()
 			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), 0);
 			glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(sizeof(float)* 4));
 			//draw to the screen
-			glDrawElements(GL_LINE_LOOP, 6, GL_UNSIGNED_BYTE, NULL);
+			glDrawElements(GL_LINES, 60, GL_UNSIGNED_BYTE, NULL);
 			glBindBuffer(GL_ARRAY_BUFFER, 0);
 			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 			//swap front and back buffers
 			glBindTexture(GL_TEXTURE_2D, 0);
-
 		}
 
 		
